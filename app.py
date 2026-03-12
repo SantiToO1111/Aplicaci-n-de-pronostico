f9fc575799dc3bed22544c4da39ef052d1ce874e
SantiToO1111 <santiago.torres.ortega@correounivalle.edu.co>
2026-03-12 09:55:52 -0500

    aplicacion de pronostico

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
+from flask import Flask, render_template, request
 import pandas as pd
 datos=pd.read_csv("venta_historicas.csv")
 #función para ejecutar un pronostico
 
-def pronosticar(historia,N):
-  datos["pronostico"]=datos["ventas"].rolling(window=N).mean().shift(1)
-  datos["Error"]=datos['pronostico']-datos['ventas']
-  datos['Error_abs']=datos['Error'].abs()
-  datos['APE']=datos['Error_abs']/datos['ventas']
-  datos["APE'"]=datos['Error_abs']/datos['pronostico']
-  datos["error_cuadrado"]=datos["Error"]*datos["Error"]
+app = Flask(__name__)
+@app.route("/")
+def home():
+  return render_template("pronosticos.html")
 
-  #medidas de error
-  MAPE=datos["APE"].mean()
-  MAPE_prima=datos["APE'"].mean()
-  MsE=datos["error_cuadrado"].mean()
-  MAE_prima=datos["Error"].mean()
-  RMSE=MsE**0.5
+@app.route('/pronosticos', methods=['GET', 'POST'])
+
+def pronosticar():
+    if request.method == 'POST':
+        N = int(request.form['N'])
+
+        datos["pronostico"]=datos["ventas"].rolling(window=N).mean().shift(1)
+        datos["Error"]=datos['pronostico']-datos['ventas']
+        datos['Error_abs']=datos['Error'].abs()
+        datos['APE']=datos['Error_abs']/datos['ventas']
+        datos["APE'"]=datos['Error_abs']/datos['pronostico']
+        datos["error_cuadrado"]=datos["Error"]*datos["Error"]
+
+        #medidas de error
+        MAPE=datos["APE"].mean()
+        MAPE_prima=datos["APE'"].mean()
+        MsE=datos["error_cuadrado"].mean()
+        MAE_prima=datos["Error"].mean()
+        RMSE=MsE**0.5
 
 
 
-  return datos,MAPE,MAPE_prima,MsE,MAE_prima,RMSE
-print(pronosticar(datos,3))
+    return datos,MAPE,MAPE_prima,MsE,MAE_prima,RMSE
+    print(pronosticar(datos,3))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
